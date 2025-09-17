521703f13ef50678e41070f4d4ca359a7008d180
Sandip <sandip.ranjit@metropolia.fi>
2025-09-17 19:52:43 +0300

    Move files from Simu_GUI to Simu_TextUI

--- Simu_GUI/src/main/java/eduni/distributions/ContinuousGenerator.java
+++
@@ -1,9 +0,0 @@
-package eduni.distributions;
-
-import eduni.distributions.Seedable;
-
-/** A continuous generator provides a double value according to the distribution it relies on.
- */
-public interface ContinuousGenerator extends Seedable {
-    double sample(); 
-}
--- Simu_GUI/src/main/java/eduni/distributions/DiscreteGenerator.java
+++
@@ -1,9 +0,0 @@
-package eduni.distributions;
-
-import eduni.distributions.Seedable;
-
-/** A continuous generator provides a long value according to the distribution it relies on.
- */
-public interface DiscreteGenerator extends Seedable {
-    long sample(); 
-}
