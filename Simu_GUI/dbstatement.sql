c274c84dcf4ad3e488c6fc4ef98d14cd34aafeab
Sandip <sandip.ranjit@metropolia.fi>
2025-10-01 22:42:15 +0300

    Database creation statement and saved

---
+++ Simu_GUI/dbstatement.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE simulationproject;
+
+CREATE USER 'dbuser'@'localhost' IDENTIFIED BY 'group7';
+
+GRANT SELECT,INSERT,UPDATE,DELETE ON simulationproject.* TO 'dbuser'@'localhost';
+
+GRANT CREATE, DROP, ALTER ON simulationproject.* TO 'dbuser'@'localhost';
