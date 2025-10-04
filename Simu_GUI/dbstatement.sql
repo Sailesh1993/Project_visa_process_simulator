27ee0f355a2c0c2fdd07ed975852a0c3d555105a
GamAungLahpai <gamt@metropolia.fi>
2025-10-04 03:25:18 +0300

    GUI version 1

--- Simu_GUI/dbstatement.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE simulationproject;
-
-CREATE USER 'dbuser'@'localhost' IDENTIFIED BY 'group7';
-
-GRANT SELECT,INSERT,UPDATE,DELETE ON simulationproject.* TO 'dbuser'@'localhost';
-
-GRANT CREATE, DROP, ALTER ON simulationproject.* TO 'dbuser'@'localhost';
--- Simu_GUI/src/main/java/simu/framework/IEngine.java
+++
@@ -1,8 +0,0 @@
-package simu.framework;
-
-/* Controller uses this interface */
-public interface IEngine { // NEW
-	public void setSimulationTime(double time);
-	public void setDelay(long time);
-	public long getDelay();
-}
