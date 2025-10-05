0175e11122fddcd3f0f0313a5e52bacdab93ba29
Sandip <sandip.ranjit@metropolia.fi>
2025-10-05 17:54:47 +0300

    Resolved merged conflicts

--- Simu_GUI/src/main/java/Main.java
+++ Simu_GUI/src/main/java/Main.java
@@ -1,36 +1,7 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import view.SimulatorUI;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            // Load the welcome page FXML
-            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/welcome.fxml"));
-            Scene scene = new Scene(loader.load());
-
-            // Setup stage
-            primaryStage.setTitle("Visa Application Processing Simulator");
-            primaryStage.setScene(scene);
-            primaryStage.setMaximized(true);
-            primaryStage.setOnCloseRequest(event -> {
-                // Cleanup resources
-                datasource.MariaDbJpaConnection.shutdown();
-                System.exit(0);
-            });
-
-            primaryStage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Failed to load the application: " + e.getMessage());
-        }
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        SimulatorUI.launch(SimulatorUI.class);
     }
 }
